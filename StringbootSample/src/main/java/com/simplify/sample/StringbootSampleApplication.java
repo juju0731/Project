a6ccdfa9137ab5ad3197ea0b86a32fd706a8e4df
JuJu <JuJu@JuJu-gram>
2019-09-16 10:45:40 +0900

    test2

--- StringbootSample/src/main/java/com/simplify/sample/StringbootSampleApplication.java
+++ StringbootSample/src/main/java/com/simplify/sample/StringbootSampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StringbootSampleApplication {
 
 	public static void main(String[] args) {
-		//TEST2
+		//TEST22
 		SpringApplication.run(StringbootSampleApplication.class, args);
 	}
 
