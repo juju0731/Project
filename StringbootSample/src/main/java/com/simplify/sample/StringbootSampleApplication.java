a7b21efbb190a8609575ee7b2d4ba8d24014b7b7
leejuhee0731 <smjh1018@gmail.com>
2019-09-18 19:58:18 +0900

    master origin add

--- StringbootSample/src/main/java/com/simplify/sample/StringbootSampleApplication.java
+++ StringbootSample/src/main/java/com/simplify/sample/StringbootSampleApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StringbootSampleApplication {
+	
+	// master origin
 
 	public static void main(String[] args) {
 
